054c79c6659bf45d9ba9f6b698ebc06c0e163e49
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2022-03-27 13:18:27 +1000

    🐦 Add twitter embed component

---
+++ components/Twitter.tsx
@@ -0,0 +1,8 @@
+import { styled } from "@stitches/react";
+import { TwitterTweetEmbed } from "react-twitter-embed";
+
+export const Tweet = styled(TwitterTweetEmbed, {
+  maxWidth: 555,
+  marginLeft: "auto",
+  marginRight: "auto",
+});
