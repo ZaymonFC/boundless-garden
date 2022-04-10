d61dc39ad305bf9ac0cd08b22cd6f1589365f5f0
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2022-04-10 16:49:48 +1000

    🔨 Extract meta into common data

---
+++ data/Meta.tsx
@@ -0,0 +1,41 @@
+const Meta = {
+  intro: {
+    id: "01",
+    title: "Intro —interlude",
+    date: new Date("2021-08"),
+    wordCount: 240,
+  },
+
+  "momentum-1": {
+    id: "487be66f-5714-4490-80a4-9cbecc93d750",
+    title: "Momentum 1",
+    date: new Date("2021-10"),
+    wordCount: 600,
+  },
+
+  "book-report/breakfast-of-champions": {
+    id: "d8b6c66a-ea2f-429d-81c7-8dcdec84f7fb",
+    title: "Book Report - Breakfast of Champions",
+    date: new Date("2021-10"),
+    wordCount: 1100,
+    tags: ["book report"],
+  },
+
+  "bytes/on-play": {
+    id: "40BDC5DF-CE8A-45FD-9B53-9408607C88AD",
+    title: "On play",
+    date: new Date("2022-02"),
+    wordCount: 520,
+    tags: ["play", "neuroscience", "life-trajectory"],
+  },
+
+  "bytes/on-courage": {
+    id: "126b1704-e798-4671-a515-ed4918d5bfda",
+    title: "On Courage",
+    date: new Date("2022-03-27"),
+    wordCount: 520,
+    tags: ["courage", "awareness"],
+  },
+};
+
+export default Meta;
