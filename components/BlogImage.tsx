90018b933db6f199c9c3a586a21e5eeb223f1f83
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2022-08-07 17:42:53 +1000

    📷 photo: Tin TIn

--- components/BlogImage.tsx
+++ components/BlogImage.tsx
@@ -1,11 +1,38 @@
-import { css, cx } from "@emotion/css";
 import Image from "next/image";
+import { styled } from "../Stitches";
 
-const blogImageStyles = css`
-  border-radius: 8px;
-`;
+const BlogCaption = styled("p", {
+  padding: 0,
+  margin: 0,
 
-type BlogImageProps = { src: any; alt: string };
-export default function BlogImage({ src, alt }: BlogImageProps) {
-  return <Image src={src} alt={alt} className={cx(blogImageStyles)} />;
+  fontSize: "$2 !important",
+  fontFamily: "$headingSerif",
+  // color: "white !important",
+
+  textAlign: "center",
+});
+
+const BlogImagePrimitive = styled(Image, {
+  borderRadius: "$4",
+});
+
+type BlogImageProps = {
+  src: any;
+  alt: string;
+  caption: boolean | string | undefined;
+  date?: string;
+};
+
+export default function BlogImage({ src, alt, caption, date }: BlogImageProps) {
+  return (
+    <div>
+      <BlogImagePrimitive src={src} alt={alt} />
+      {caption && (
+        <BlogCaption>
+          {caption === true ? alt : caption}
+          {date && <em> {date}</em>}
+        </BlogCaption>
+      )}
+    </div>
+  );
 }
